4c172a810cd31013acc974852c38899bbecf277d
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-05-25 10:37:20 -0500

    fixed getGenres bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  // const [genres, setGenres] = useState(null);
   // const fetchData = async () => {
-  //   const response = awatit fetch()
-  // }
+  //   const response = await fetch("./netlify/functions/getGenres");
+  //   const responseBody = await response.json();
+  //   console.log(responseBody);
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
   return <></>;
 }
 
