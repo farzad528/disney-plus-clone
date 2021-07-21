ff0628c183e8ebab5b3a248f096408e8e5f74681
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-07-21 05:43:56 -0500

    added images and videos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    )
-}
+  return (
+    <Nav>
+      <Logo />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
+};
 
-export default Header
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
