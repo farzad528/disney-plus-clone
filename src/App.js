1794f6a22b186dec022d2886dd9bee97eae25eed
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-07-21 05:26:23 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import HeroSection from "./components/HeroSection";
-import NavBar from "./components/NavBar";
-import Section from "./components/Section";
-
-function App() {
-  const genreIncrement = 4;
-  const [genres, setGenres] = useState(null);
-  const [limit, setLimit] = useState(genreIncrement);
-
-  const fetchData = async () => {
-    const response = await fetch("/.netlify/functions/getGenres", {
-      method: "POST",
-      body: limit,
-    });
-    const responseBody = await response.json();
-    setGenres(responseBody.data.reference_list.values);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, [limit]);
+const App = () => {
   return (
-    <>
-      <NavBar />
-      <HeroSection />
-      {genres && (
-        <div className="container">
-          {Object.values(genres).map((genre, index) => (
-            <Section key={index} genre={genre.value} />
-          ))}
-        </div>
-      )}
-      <div
-        className="page-end"
-        onMouseEnter={() => {
-          setLimit(limit + genreIncrement);
-        }}
-      />
-    </>
-  );
+    <div className="app">
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
+
