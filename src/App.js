e05afe0fc5dddcb1c14f9eabfa17bb3aaf66942f
Farzad Sunavala <40604067+farzad528@users.noreply.github.com>
2021-07-21 05:31:25 -0500

    styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Header from "./components/Header"
+
 const App = () => {
   return (
     <div className="app">
-      
+      {/* Header */}
+      <Header />
+      {/* Carousel */}
+      {/* Viewer/Categories */}
+      {/* Movies List */}
     </div>
   )
 }
